7fbe1b7cbedf061d75fe9fdd35041a49c1da23ef
Nicci Williams <nicci@nicciwill.com>
2018-02-20 15:14:56 -0500

    mvp api endpoints and mLab connection working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -6,34 +8,38 @@ const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+const apiRouter = require('./api-router');
+
 const app = express();
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
 );
 
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
+  cors({
+    origin: CLIENT_ORIGIN
+  })
 );
 
+app.use('/', apiRouter);
+
 function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
-        })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
-        });
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+  dbConnect();
+  runServer();
 }
 
 module.exports = {app};
