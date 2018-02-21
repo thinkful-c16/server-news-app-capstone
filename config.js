c2d1bcfef4d64f408dd95155969bf30f10af0b87
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-21 15:16:59 -0600

    removed typo in db url

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://cohort16>:th1nkful@ds143678.mlab.com:43678/news-app-capstone',
+        process.env.DATABASE_URL || 'mongodb://cohort16:th1nkful@ds143678.mlab.com:43678/news-app-capstone',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/newsapp-backend-test',
