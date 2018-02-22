7b0bbef03771cf05ad238b8ff8ee386fa3e7de32
Kayla Webb <kaylarwebb@gmail.com>
2018-02-22 09:45:52 -0500

    Able to successfully query for the user name and email after verifying fb token

--- auth/router.js
+++ auth/router.js
@@ -35,28 +35,44 @@ router.post('/refresh', jwtAuth, (req, res) => {
 router.post('/facebook', (req, res) => {
   //check to see if token is valid
   const userToken = req.body.token;
+  console.log('user token', userToken);
   fetch(`https://graph.facebook.com/debug_token?input_token=${userToken}&access_token=${FACEBOOK_APP_TOKEN}`)
     .then(response => response.json())
     .then(data => {
-      const { user_id } = data;
-      console.log(user_id);
-      User.findOne({'facebook.id': user_id}, function(err, user) {
-        if (err) {
-          return err;
-        }
-        if (user) {
-          const authToken = createAuthToken(user.apiRepr());
-          res.json({authToken});
-        } else {
-          //need to query the profile for name and email, save the fb token to the db
-          fetch(`https://graph.facebook.com/${user_id}`)
-            .then(response => response.json())
-            .then(data => console.log(data));
-
-        } 
-      });
-
+      // console.log('Data', data.data)
+      const { user_id } = data.data;
+      console.log('User id', user_id);
+      fetch(`https://graph.facebook.com/${user_id}?access_token=${FACEBOOK_APP_TOKEN}&fields=id,first_name,last_name,email`)
+        .then(response => response.json())
+        .then(userData => console.log('User data', userData));
     });
+  res.send('ok');
+  // console.log(user_id);
+  // let user;
+  // return User.findOne({'facebook.id': user_id})
+  //   .then(_user => {
+  //     user = _user;
+  //     if(!user) {
+  //       User.create({
+  //         name,
+  //         email
+  //       });
+  //     }
+  //     .then(user => {
+  //       const authToken = createAuthToken(user.apiRepr());
+  //       return res.status(201).json({authToken})
+  //     })
+
+
+  //need to query the profile for name and email, save the fb token to the db
+  // fetch(`https://graph.facebook.com/${user_id}`)
+  //   .then(response => response.json())
+  //   .then(data => console.log(data));
+        
+
+        
+    
+
   //check to see if fb id exists for a user in our database
   //if the user doesnt exist create the users
   //create the auth token and send back the auth token
