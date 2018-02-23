48f98c69d80f872d88948c6f7fe4880e70fe4032
Kayla Webb <kaylarwebb@gmail.com>
2018-02-23 15:00:52 -0500

    Pseudocode the post endpoint for collections

--- collections/router.js
+++ collections/router.js
@@ -16,20 +16,34 @@ router.use(jsonParser);
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
-//post an article to a specific collection via id
-//
+//post a collection to the user record
+//find the user by id
+//destructure the collection title from the collection object
+//return the collection id and location to the user
 
 router.post('/', jwtAuth, (req, res) => {
-  const user = req.user;
-  console.log(user);
-  User.findById(user.id)
+  const userId = req.user.id;
+  // console.log(user);
+  User.findById(userId)
     .then(user => {
       console.log(user)
       user.collections = col1;
       user.save();
-      console.log('User after new collection', user)
+      console.log('User after new collection', user);
     });
   res.send('ok');
+});
+
+//article from the client is an object
+//use params to get collection id
+//find the user by id
+//find the collection by id
+//destructure the article object
+//save the article props to the collection
+router.post('/:collections', jwtAuth, (req, res) => {
+  const collectionId = req.params.id;
+  const userId = req.user.id;
+
 });
 
 module.exports = { router };
