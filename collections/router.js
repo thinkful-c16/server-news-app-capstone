e50b1ca76c96e565845620fd64b81c838ed6765d
Ryan Sabo <sabora920@gmail.com>
2018-02-26 11:21:26 -0500

    finished the put and delete endpoints but have not tested

--- collections/router.js
+++ collections/router.js
@@ -6,6 +6,8 @@ const { User } = require('../users/model');
 const router = express.Router();
 const jsonParser = bodyParser.json();
 const passport = require('passport');
+const { collection1, collection2, } = require('./dummy-data');
+
 
 const jwtAuth = passport.authenticate('jwt', { session: false })
 
@@ -25,6 +27,58 @@ router.get('/', jwtAuth, (req, res) => {
             });
         });
     res.send('ok')
+})
+
+router.post('/', jwtAuth, (req, res) => {
+    const userId = req.user.id;
+    User.findById(userId)
+      .then(user => {
+        console.log(user)
+        user.collections = collection2;
+        user.save();
+        console.log('User after new collection', user);
+      });
+    res.send('ok');
+  });
+
+router.put('/:collections', jwtAuth, (req, res) => {
+    const collectionId = req.params.collections;
+    const user = req.user;    
+    
+    if(!(collectionId && req.body.id === req.body.id)){
+        res.status(400).json({
+          error: 'Request path ID and request body ID must match'
+        });
+      }
+
+    const updated = {};
+    const updatableFields = ['title']
+    updatableFields.forEach(field => {
+        if(field in req.body){
+            udated[field] = req.body[field]
+        }
+    })
+    
+    console.log('req.params.id', collectionId)
+    
+    User.findByIdAndUpdate(collectionId, {$set: updated}, {new: true})
+        .then(updatedCollection => {
+            console.log(_user.collections)
+            res.status(201).json(updatedCollection);
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Something went wrong!'})
+        })
+})
+
+router.delete('/:collections/:id', jwtAuth, (req, res) => {
+    User.update(
+        {_id: req.params.collections},
+        { $pull: { 'collections':{_id: req.params.id} } }
+    )
+        .then(result => {
+            res.status(204).end();
+        })
 })
 
 module.exports = { router };
